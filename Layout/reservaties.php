5f6e6dc6f7bd1c4b99181b63d92ae745abe2a233
r0614393 <r0614393@N-A213-107566.local.thomasmore.be>
2019-11-20 16:23:44 +0100

    Layout

---
+++ Layout/reservaties.php
@@ -0,0 +1,75 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<title>Reservaties</title>
+		
+		<!--FONTS-->
+		<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+	</head>
+	<body>
+		<div class="page wide">
+			<h1>Reservaties:</h1>
+			<table class='calendar'>
+				<tr>
+					<th>Maandag</th>
+					<th>Dinsdag</th>
+					<th>Woensdag</th>
+					<th>Donderdag</th>
+					<th>Vrijdag</th>
+					<th>Zaterdag</th>
+					<th>Zondag</th>
+				</tr>
+				<tr>
+					<td class="niet_maand">28</td>
+					<td class="niet_maand">29</td>
+					<td class="niet_maand">30</td>
+					<td class="niet_maand">31</td>
+					<td>1
+						<div class='uur vrij'>11:00 - Vrij</div>
+						<div class='uur bezet'>12:00 - R0614393</div>
+						<div class='uur vrij'>13:00 - Vrij</div>
+					</td>
+					<td class="niet_open">2</td>
+					<td class="niet_open">3</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>5</td>
+					<td>6</td>
+					<td>7</td>
+					<td>8</td>
+					<td class="niet_open">9</td>
+					<td class="niet_open">10</td>
+				</tr>
+				<tr>
+					<td>11</td>
+					<td>12</td>
+					<td>13</td>
+					<td>14</td>
+					<td>15</td>
+					<td class="niet_open">16</td>
+					<td class="niet_open">17</td>
+				</tr>
+				<tr>
+					<td>18</td>
+					<td>19</td>
+					<td>20</td>
+					<td>21</td>
+					<td>22</td>
+					<td class="niet_open">23</td>
+					<td class="niet_open">24</td>
+				</tr>
+				<tr>
+					<td>25</td>
+					<td>26</td>
+					<td>27</td>
+					<td>28</td>
+					<td>29</td>
+					<td class="niet_open">30</td>
+					<td class="niet_open niet_maand">1</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
